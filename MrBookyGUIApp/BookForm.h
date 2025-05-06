aeea9a6552ecef407b64cf425477ac1753bfb953
HP <HP@LAPTOP-3ACUNC67>
2025-05-05 20:25:23 -0500

    Modificacion 1

---
+++ MrBookyGUIApp/BookForm.h
@@ -0,0 +1,203 @@
+#pragma once
+
+namespace MrBookyGUIApp {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de BookForm
+	/// </summary>
+	public ref class BookForm : public System::Windows::Forms::Form
+	{
+	public:
+		BookForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~BookForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->label1->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 30, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::Cornsilk;
+			this->label1->Location = System::Drawing::Point(12, 18);
+			this->label1->Name = L"label1";
+			this->label1->RightToLeft = System::Windows::Forms::RightToLeft::Yes;
+			this->label1->Size = System::Drawing::Size(546, 125);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Bienvenido Sistema de Búsqueda MrBooky";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::TopRight;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::SystemColors::ControlLightLight;
+			this->label2->Location = System::Drawing::Point(25, 211);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(247, 29);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Busqueda Avanzada";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->ForeColor = System::Drawing::SystemColors::ControlLightLight;
+			this->label3->Location = System::Drawing::Point(27, 270);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(38, 16);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"Autor";
+			this->label3->Click += gcnew System::EventHandler(this, &BookForm::label3_Click);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->ForeColor = System::Drawing::SystemColors::ControlLightLight;
+			this->label4->Location = System::Drawing::Point(27, 311);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(56, 16);
+			this->label4->TabIndex = 3;
+			this->label4->Text = L"Editorial";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->ForeColor = System::Drawing::SystemColors::ControlLightLight;
+			this->label5->Location = System::Drawing::Point(27, 357);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(52, 16);
+			this->label5->TabIndex = 4;
+			this->label5->Text = L"Género";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(101, 270);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(144, 22);
+			this->textBox1->TabIndex = 5;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(101, 311);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(144, 22);
+			this->textBox2->TabIndex = 6;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(101, 357);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(144, 22);
+			this->textBox3->TabIndex = 7;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(30, 157);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(579, 22);
+			this->textBox4->TabIndex = 8;
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(631, 155);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(60, 24);
+			this->richTextBox1->TabIndex = 9;
+			this->richTextBox1->Text = L"Buscar";
+			// 
+			// BookForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->ClientSize = System::Drawing::Size(858, 452);
+			this->Controls->Add(this->richTextBox1);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"BookForm";
+			this->Text = L"Búsqueda con filtros";
+			this->Load += gcnew System::EventHandler(this, &BookForm::BookForm_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void BookForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void label3_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
+}
---
+++ MrBookyGUIApp/UserForm.h
@@ -0,0 +1,161 @@
+#pragma once
+
+namespace MrBookyGUIApp {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de UserForm
+	/// </summary>
+	public ref class UserForm : public System::Windows::Forms::Form
+	{
+	public:
+		UserForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~UserForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+	private: System::Windows::Forms::LinkLabel^ linkLabel1;
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->linkLabel1 = (gcnew System::Windows::Forms::LinkLabel());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 36, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::SystemColors::ControlLightLight;
+			this->label1->Location = System::Drawing::Point(245, 89);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(287, 62);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Mr. Booky";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->ForeColor = System::Drawing::SystemColors::ControlLightLight;
+			this->label2->Location = System::Drawing::Point(156, 194);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(60, 16);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Usuario: ";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->ForeColor = System::Drawing::SystemColors::ControlLightLight;
+			this->label3->Location = System::Drawing::Point(156, 236);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(79, 16);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"Contraseña:";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(267, 191);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(265, 22);
+			this->textBox1->TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(267, 233);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(265, 22);
+			this->textBox2->TabIndex = 4;
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(335, 293);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(72, 24);
+			this->richTextBox1->TabIndex = 5;
+			this->richTextBox1->Text = L"Ingresar";
+			this->richTextBox1->TextChanged += gcnew System::EventHandler(this, &UserForm::richTextBox1_TextChanged);
+			// 
+			// linkLabel1
+			// 
+			this->linkLabel1->AutoSize = true;
+			this->linkLabel1->LinkColor = System::Drawing::Color::Teal;
+			this->linkLabel1->Location = System::Drawing::Point(308, 340);
+			this->linkLabel1->Name = L"linkLabel1";
+			this->linkLabel1->Size = System::Drawing::Size(132, 16);
+			this->linkLabel1->TabIndex = 6;
+			this->linkLabel1->TabStop = true;
+			this->linkLabel1->Text = L"¿No está registrado\?";
+			this->linkLabel1->LinkClicked += gcnew System::Windows::Forms::LinkLabelLinkClickedEventHandler(this, &UserForm::linkLabel1_LinkClicked);
+			// 
+			// UserForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->ClientSize = System::Drawing::Size(792, 466);
+			this->Controls->Add(this->linkLabel1);
+			this->Controls->Add(this->richTextBox1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"UserForm";
+			this->Text = L"UserForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void richTextBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void linkLabel1_LinkClicked(System::Object^ sender, System::Windows::Forms::LinkLabelLinkClickedEventArgs^ e) {
+}
+};
+}
