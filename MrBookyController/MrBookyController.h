20d3deab97ad9109a69a7d78c71c513bc046d9f0
Facundo <a20222021@pucp.edu.pe>
2025-05-09 15:49:37 -0500

    Actualización de la capa Controller

--- MrBookyController/MrBookyController.h
+++ MrBookyController/MrBookyController.h
@@ -9,6 +9,11 @@ namespace MrBookyController {
 	{
 		private:
 			static List<Book^>^ books = gcnew List<Book^>();
+			static List<DeliveryRobot^>^ robots = gcnew List<DeliveryRobot^>();
+			static List<Library^>^ libraries = gcnew List<Library^>();
+			static List<User^>^ users = gcnew List<User^>();
+			static List<Loan^>^ loans = gcnew List<Loan^>();
+			static List<CartItem^>^ cartItems = gcnew List<CartItem^>();
 		// TODO: Agregue aqu√≠ los m√©todos de esta clase.
 		public:
 			// M√©todos CRUD para Book
@@ -17,5 +22,41 @@ namespace MrBookyController {
 			static Book^ SearchBook(int index);
 			static int UpdateBook(Book^ book);
 			static int DeleteBook(int index);
+
+			// MÈtodos CRUD para DeliveryRobot
+			static int AddRobot(DeliveryRobot^ robot);
+			static List<DeliveryRobot^>^ GetRobots();
+			static DeliveryRobot^ SearchRobot(int robotId);
+			static int UpdateRobot(DeliveryRobot^ robot);
+			static int DeleteRobot(int robotId);
+
+			// MÈtodos CRUD para Library
+			static int AddLibrary(Library^ library);
+			static List<Library^>^ GetLibraries();
+			static Library^ SearchLibrary(int libraryId);
+			static int UpdateLibrary(Library^ library);
+			static int DeleteLibrary(int libraryId);
+
+			// MÈtodos CRUD para User
+			static int AddUser(User^ user);
+			static List<User^>^ GetUsers();
+			static User^ SearchUser(int userId);
+			static int UpdateUser(User^ user);
+			static int DeleteUser(int userId);
+
+			// MÈtodos CRUD para Loan
+			static int AddLoan(Loan^ loan);
+			static List<Loan^>^ GetLoans();
+			static Loan^ SearchLoan(int loanId);
+			static int UpdateLoan(Loan^ loan);
+			static int DeleteLoan(int loanId);
+
+			// MÈtodos CRUD para CartItem
+			static int AddCartItem(CartItem^ cartItem);
+			static List<CartItem^>^ GetCartItems();
+			static CartItem^ SearchCartItem(int cartItemId);
+			static int UpdateCartItem(CartItem^ cartItem);
+			static int DeleteCartItem(int cartItemId);
+
 	};
 }
