ef77840e9adbd75fb0fab84eacfd0e7352b91372
HP <HP@LAPTOP-3ACUNC67>
2025-05-12 20:37:02 -0500

    Corrección de main duplicado MainteanceBook (eliminado), y MainForm

--- MrBookyGUIApp/MaintenanceBook.cpp
+++ MrBookyGUIApp/MaintenanceBook.cpp
@@ -1,9 +1,9 @@
 #include "MaintenanceBook.h"
-using namespace System;
-using namespace MrBookyGUIApp;
-
-int main() {
-	MaintenanceBook^ maintenancebook = gcnew MaintenanceBook();
-	Application::Run(maintenancebook);
-	return 0;
-}
+//using namespace System;
+//using namespace MrBookyGUIApp;
+//
+//int main() {
+//	MaintenanceBook^ maintenancebook = gcnew MaintenanceBook();
+//	Application::Run(maintenancebook);
+//	return 0;
+//}
