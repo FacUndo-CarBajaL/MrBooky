9528f4190fd78553839894c8e4e4c4078fedd21c
Facundo <a20222021@pucp.edu.pe>
2025-06-05 21:58:45 -0500

    Funcionalidad busqueda de libros

--- MrBookyGUIApp/FoundBookForm.h
+++ MrBookyGUIApp/FoundBookForm.h
@@ -143,195 +143,123 @@ namespace MrBookyGUIApp {
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pbBook))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dvgReviews))->BeginInit();
 			this->SuspendLayout();
-			// 
-			// label1
-			// 
 			this->label1->AutoSize = true;
 			this->label1->Location = System::Drawing::Point(31, 28);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(47, 20);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Título";
-			// 
-			// label2
-			// 
 			this->label2->AutoSize = true;
 			this->label2->Location = System::Drawing::Point(31, 86);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(48, 20);
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"Autor";
-			// 
-			// label3
-			// 
 			this->label3->AutoSize = true;
 			this->label3->Location = System::Drawing::Point(31, 147);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(63, 20);
 			this->label3->TabIndex = 2;
 			this->label3->Text = L"Genero";
-			// 
-			// label4
-			// 
 			this->label4->AutoSize = true;
 			this->label4->Location = System::Drawing::Point(321, 28);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(107, 20);
 			this->label4->TabIndex = 3;
 			this->label4->Text = L"Disponibilidad";
-			// 
-			// txtTitle
-			// 
 			this->txtTitle->Location = System::Drawing::Point(35, 51);
 			this->txtTitle->Name = L"txtTitle";
 			this->txtTitle->Size = System::Drawing::Size(108, 26);
 			this->txtTitle->TabIndex = 4;
-			// 
-			// txtAuthor
-			// 
 			this->txtAuthor->Location = System::Drawing::Point(35, 109);
 			this->txtAuthor->Name = L"txtAuthor";
 			this->txtAuthor->Size = System::Drawing::Size(108, 26);
 			this->txtAuthor->TabIndex = 5;
-			// 
-			// txtGenre
-			// 
 			this->txtGenre->Location = System::Drawing::Point(35, 170);
 			this->txtGenre->Name = L"txtGenre";
 			this->txtGenre->Size = System::Drawing::Size(108, 26);
 			this->txtGenre->TabIndex = 6;
-			// 
-			// txtAvailability
-			// 
 			this->txtAvailability->Location = System::Drawing::Point(325, 51);
 			this->txtAvailability->Name = L"txtAvailability";
 			this->txtAvailability->Size = System::Drawing::Size(108, 26);
 			this->txtAvailability->TabIndex = 7;
-			// 
-			// txtPublisher
-			// 
 			this->txtPublisher->Location = System::Drawing::Point(325, 109);
 			this->txtPublisher->Name = L"txtPublisher";
 			this->txtPublisher->Size = System::Drawing::Size(108, 26);
 			this->txtPublisher->TabIndex = 8;
-			// 
-			// label5
-			// 
 			this->label5->AutoSize = true;
 			this->label5->Location = System::Drawing::Point(321, 86);
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(66, 20);
 			this->label5->TabIndex = 9;
 			this->label5->Text = L"Editorial";
-			// 
-			// label6
-			// 
 			this->label6->AutoSize = true;
 			this->label6->Location = System::Drawing::Point(321, 147);
 			this->label6->Name = L"label6";
 			this->label6->Size = System::Drawing::Size(122, 20);
 			this->label6->TabIndex = 10;
 			this->label6->Text = L"Año Publicación";
-			// 
-			// txtReleaseYear
-			// 
 			this->txtReleaseYear->Location = System::Drawing::Point(325, 170);
 			this->txtReleaseYear->Name = L"txtReleaseYear";
 			this->txtReleaseYear->Size = System::Drawing::Size(108, 26);
 			this->txtReleaseYear->TabIndex = 11;
-			// 
-			// txtStock
-			// 
 			this->txtStock->Location = System::Drawing::Point(35, 233);
 			this->txtStock->Name = L"txtStock";
 			this->txtStock->Size = System::Drawing::Size(100, 26);
 			this->txtStock->TabIndex = 12;
-			// 
-			// label7
-			// 
 			this->label7->AutoSize = true;
 			this->label7->Location = System::Drawing::Point(31, 210);
 			this->label7->Name = L"label7";
 			this->label7->Size = System::Drawing::Size(50, 20);
 			this->label7->TabIndex = 13;
 			this->label7->Text = L"Stock";
-			// 
-			// label8
-			// 
 			this->label8->AutoSize = true;
 			this->label8->Location = System::Drawing::Point(321, 210);
 			this->label8->Name = L"label8";
 			this->label8->Size = System::Drawing::Size(92, 20);
 			this->label8->TabIndex = 14;
 			this->label8->Text = L"Descripcion";
-			// 
-			// txtDescription
-			// 
 			this->txtDescription->Location = System::Drawing::Point(325, 233);
 			this->txtDescription->Name = L"txtDescription";
 			this->txtDescription->Size = System::Drawing::Size(108, 26);
 			this->txtDescription->TabIndex = 15;
-			// 
-			// pbBook
-			// 
 			this->pbBook->Location = System::Drawing::Point(597, 51);
 			this->pbBook->Name = L"pbBook";
 			this->pbBook->Size = System::Drawing::Size(149, 138);
 			this->pbBook->TabIndex = 16;
 			this->pbBook->TabStop = false;
-			// 
-			// label9
-			// 
 			this->label9->AutoSize = true;
 			this->label9->Location = System::Drawing::Point(638, 207);
 			this->label9->Name = L"label9";
 			this->label9->Size = System::Drawing::Size(65, 20);
 			this->label9->TabIndex = 17;
 			this->label9->Text = L"Portada";
-			// 
-			// label10
-			// 
 			this->label10->AutoSize = true;
 			this->label10->Location = System::Drawing::Point(31, 272);
 			this->label10->Name = L"label10";
 			this->label10->Size = System::Drawing::Size(154, 20);
 			this->label10->TabIndex = 18;
 			this->label10->Text = L"Tiempo de prestamo";
-			// 
-			// txtLoanTime
-			// 
 			this->txtLoanTime->Location = System::Drawing::Point(35, 295);
 			this->txtLoanTime->Name = L"txtLoanTime";
 			this->txtLoanTime->Size = System::Drawing::Size(100, 26);
 			this->txtLoanTime->TabIndex = 19;
-			// 
-			// txtWeight
-			// 
 			this->txtWeight->Location = System::Drawing::Point(325, 295);
 			this->txtWeight->Name = L"txtWeight";
 			this->txtWeight->Size = System::Drawing::Size(108, 26);
 			this->txtWeight->TabIndex = 20;
-			// 
-			// label11
-			// 
 			this->label11->AutoSize = true;
 			this->label11->Location = System::Drawing::Point(321, 272);
 			this->label11->Name = L"label11";
 			this->label11->Size = System::Drawing::Size(76, 20);
 			this->label11->TabIndex = 21;
 			this->label11->Text = L"Peso (kg)";
-			// 
-			// label12
-			// 
 			this->label12->AutoSize = true;
 			this->label12->Location = System::Drawing::Point(31, 334);
 			this->label12->Name = L"label12";
 			this->label12->Size = System::Drawing::Size(99, 20);
 			this->label12->TabIndex = 22;
 			this->label12->Text = L"Comentarios";
-			// 
-			// dvgReviews
-			// 
 			this->dvgReviews->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
 			this->dvgReviews->Location = System::Drawing::Point(35, 357);
 			this->dvgReviews->Name = L"dvgReviews";
@@ -339,9 +267,6 @@ namespace MrBookyGUIApp {
 			this->dvgReviews->RowTemplate->Height = 28;
 			this->dvgReviews->Size = System::Drawing::Size(768, 150);
 			this->dvgReviews->TabIndex = 23;
-			// 
-			// FoundBookForm
-			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->AutoScroll = true;
@@ -411,9 +336,7 @@ namespace MrBookyGUIApp {
 			dvgReviews->Rows->Clear(); // Limpiar el DataGridView antes de mostrar los resultados
 			for each (String^ review in book->Reviews) {
 				int rowIndex = dvgReviews->Rows->Add();
-				dvgReviews->Rows[rowIndex]->Cells[0]->Value = review->ReviewerName;
-				dvgReviews->Rows[rowIndex]->Cells[1]->Value = review->Rating;
-				dvgReviews->Rows[rowIndex]->Cells[2]->Value = review->Comment;
+				dvgReviews->Rows[rowIndex]->Cells[0]->Value = review;
 			}
 
 		}
