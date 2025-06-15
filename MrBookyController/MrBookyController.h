dde3157ac33a9c238d7712d6e8d5d83a01971b6d
Facundo <a20222021@pucp.edu.pe>
2025-06-15 13:02:12 -0500

    Agregar bibliotecas para comunicación UART

--- MrBookyController/MrBookyController.h
+++ MrBookyController/MrBookyController.h
@@ -2,6 +2,8 @@
 
 using namespace System;
 using namespace System::Collections::Generic;
+using namespace System::IO;
+using namespace System::IO::Ports;
 using namespace MrBookyModel;
 using namespace MrBookyPersistance;
 
