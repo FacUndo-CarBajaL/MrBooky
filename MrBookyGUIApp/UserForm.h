1b84a898bea6be299ebd944517787694d7fc2368
Facundo <a20222021@pucp.edu.pe>
2025-07-10 18:23:13 -0500

    Cambio de GUI

--- MrBookyGUIApp/UserForm.h
+++ MrBookyGUIApp/UserForm.h
@@ -78,57 +78,64 @@ namespace MrBookyGUIApp {
 			// label1
 			// 
 			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::Transparent;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 36, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->ForeColor = System::Drawing::SystemColors::ControlLightLight;
-			this->label1->Location = System::Drawing::Point(245, 89);
+			this->label1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(33)), static_cast<System::Int32>(static_cast<System::Byte>(33)),
+				static_cast<System::Int32>(static_cast<System::Byte>(33)));
+			this->label1->Location = System::Drawing::Point(276, 111);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(287, 62);
+			this->label1->Size = System::Drawing::Size(341, 75);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Mr. Booky";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->ForeColor = System::Drawing::SystemColors::ControlLightLight;
-			this->label2->Location = System::Drawing::Point(156, 194);
+			this->label2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(33)), static_cast<System::Int32>(static_cast<System::Byte>(33)),
+				static_cast<System::Int32>(static_cast<System::Byte>(33)));
+			this->label2->Location = System::Drawing::Point(176, 242);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(60, 16);
+			this->label2->Size = System::Drawing::Size(72, 20);
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"Usuario: ";
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->ForeColor = System::Drawing::SystemColors::ControlLightLight;
-			this->label3->Location = System::Drawing::Point(156, 236);
+			this->label3->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(33)), static_cast<System::Int32>(static_cast<System::Byte>(33)),
+				static_cast<System::Int32>(static_cast<System::Byte>(33)));
+			this->label3->Location = System::Drawing::Point(176, 295);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(79, 16);
+			this->label3->Size = System::Drawing::Size(96, 20);
 			this->label3->TabIndex = 2;
 			this->label3->Text = L"Contraseña:";
 			// 
 			// txtUserName
 			// 
-			this->txtUserName->Location = System::Drawing::Point(267, 191);
+			this->txtUserName->Location = System::Drawing::Point(300, 236);
+			this->txtUserName->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
 			this->txtUserName->Name = L"txtUserName";
-			this->txtUserName->Size = System::Drawing::Size(265, 22);
+			this->txtUserName->Size = System::Drawing::Size(298, 26);
 			this->txtUserName->TabIndex = 3;
 			// 
 			// txtUserPassword
 			// 
-			this->txtUserPassword->Location = System::Drawing::Point(267, 233);
+			this->txtUserPassword->Location = System::Drawing::Point(300, 291);
+			this->txtUserPassword->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
 			this->txtUserPassword->Name = L"txtUserPassword";
 			this->txtUserPassword->PasswordChar = '*';
-			this->txtUserPassword->Size = System::Drawing::Size(265, 22);
+			this->txtUserPassword->Size = System::Drawing::Size(298, 26);
 			this->txtUserPassword->TabIndex = 4;
 			// 
 			// linkLabel1
 			// 
+			this->linkLabel1->ActiveLinkColor = System::Drawing::Color::RosyBrown;
 			this->linkLabel1->AutoSize = true;
 			this->linkLabel1->LinkColor = System::Drawing::Color::Teal;
-			this->linkLabel1->Location = System::Drawing::Point(308, 340);
+			this->linkLabel1->Location = System::Drawing::Point(349, 428);
 			this->linkLabel1->Name = L"linkLabel1";
-			this->linkLabel1->Size = System::Drawing::Size(132, 16);
+			this->linkLabel1->Size = System::Drawing::Size(157, 20);
 			this->linkLabel1->TabIndex = 6;
 			this->linkLabel1->TabStop = true;
 			this->linkLabel1->Text = L"¿No está registrado\?";
@@ -136,17 +143,23 @@ namespace MrBookyGUIApp {
 			// 
 			// btnIngresar
 			// 
-			this->btnIngresar->BackColor = System::Drawing::Color::White;
+			this->btnIngresar->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(56)), static_cast<System::Int32>(static_cast<System::Byte>(142)),
+				static_cast<System::Int32>(static_cast<System::Byte>(60)));
 			this->btnIngresar->FlatAppearance->BorderColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)),
 				static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(64)));
 			this->btnIngresar->FlatAppearance->MouseDownBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)),
 				static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)));
 			this->btnIngresar->FlatAppearance->MouseOverBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(128)),
 				static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(255)));
-			this->btnIngresar->Location = System::Drawing::Point(327, 294);
+			this->btnIngresar->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->btnIngresar->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnIngresar->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(33)), static_cast<System::Int32>(static_cast<System::Byte>(33)),
+				static_cast<System::Int32>(static_cast<System::Byte>(33)));
+			this->btnIngresar->Location = System::Drawing::Point(366, 353);
 			this->btnIngresar->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->btnIngresar->Name = L"btnIngresar";
-			this->btnIngresar->Size = System::Drawing::Size(95, 25);
+			this->btnIngresar->Size = System::Drawing::Size(120, 50);
 			this->btnIngresar->TabIndex = 8;
 			this->btnIngresar->Text = L"Ingresar";
 			this->btnIngresar->UseVisualStyleBackColor = false;
@@ -154,11 +167,10 @@ namespace MrBookyGUIApp {
 			// 
 			// UserForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
-				static_cast<System::Int32>(static_cast<System::Byte>(64)));
-			this->ClientSize = System::Drawing::Size(792, 466);
+			this->BackColor = System::Drawing::Color::WhiteSmoke;
+			this->ClientSize = System::Drawing::Size(891, 582);
 			this->Controls->Add(this->btnIngresar);
 			this->Controls->Add(this->linkLabel1);
 			this->Controls->Add(this->txtUserPassword);
@@ -166,6 +178,7 @@ namespace MrBookyGUIApp {
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
+			this->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
 			this->Name = L"UserForm";
 			this->Text = L"UserForm";
 			this->Load += gcnew System::EventHandler(this, &UserForm::UserForm_Load);
